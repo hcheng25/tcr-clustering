7a115ee4a79c5f1ad746c74d79ad53e4fc50edee
HC <181307191+hcheng25@users.noreply.github.com>
2026-01-14 22:33:56 -0800

    updated load_norms.R to not take up duplicate memory to load datasets

--- functions/load_norms.R
+++ functions/load_norms.R
@@ -1,8 +1,4 @@
-# load all sets of normalized data
-freq <- readRDS('rds/freq_neg7.RDS')
-freq_plus_one_method <- readRDS('rds/freq_plus_one_method.RDS')
-
 # put all sets into a list for iterating
-all_sets <- list(freq = freq,
-                 freq_plus_one_method = freq_plus_one_method
+all_sets <- list(freq = readRDS('rds/freq_neg7.RDS'),
+                 freq_plus_one_method = readRDS('rds/freq_plus_one_method.RDS')
                  )
