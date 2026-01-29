1cfe354b59b7f3dc017f88d3057dbaf5acf4e1e0
HC <181307191+hcheng25@users.noreply.github.com>
2026-01-29 01:38:34 -0800

    kmeans edited - removed elbow plot method, use gap statistics method

--- scripts/kmeans.R
+++ scripts/kmeans.R
@@ -11,60 +11,35 @@ set.seed(42)
 # load all normalized data sets
 source('functions/load_norms.R')
 
-# use elbow method
-# initialize best_k_elbow to record best k as recorded by second derivative method below
-best_k_elbow <- integer(length(all_sets))
-
-# iterate from k=1 to k=10 to find WSS and plot elbow plots
+# ----- using gap statistic to select number of clusters -----
 for (ii in seq_along(all_sets)){
-  wss <- 1:10 # initialize within-cluster sum of squares
-  for (k in 1:10){
-    km <- kmeans(all_sets[[ii]], centers = k, nstart = 25)
-    wss[k] <- km$tot.withinss
+  test_frame <- all_sets[[ii]]
+
+  # use gap statistic to select number of clusters
+  gap_stat <- clusGap(x = test_frame,
+                      FUN = kmeans,
+                      K.max = 30,
+                      nstart = 25,
+                      B = 50 # adjust to lower number if just testing code
+                      )
+  # print(gap_stat, method='Tibs2001SEmax')
+  optimal_k <- maxSE(gap_stat$Tab[, "gap"],
+                     gap_stat$Tab[, "SE.sim"],
+                     method = "firstSEmax")
+  # plot(gap_stat)
+  # note: trouble with convergence
+  
+  km <- kmeans(test_frame, centers = optimal_k, nstart = 25)
+  set_with_clusters <- test_frame
+  set_with_clusters$cluster <- as.factor(km$cluster)
+  for (jj in seq_along(levels(set_with_clusters$cluster))){
+    levels(set_with_clusters$cluster)[jj] <- paste0('Cluster_',
+                                                    levels(set_with_clusters$cluster)[jj],
+                                                    ' (n=',
+                                                    table(set_with_clusters$cluster)[[jj]],
+                                                    ')')
   }
-  
-  wss_plot <- data.frame(x = 1:10,
-                         y = wss)
-  
-  # use second derivative to detect which point corresponds to elbow
-  first_diff <- diff(wss)
-  second_diff <- diff(first_diff)
-  # first local minimum of second diff i.e. where the rate of decrease first reaches a local minimum
-  # compare each value of the second differential to the previous value
-  elbow_pt <- which(second_diff[-1] > second_diff[-length(second_diff)])[1] + 1
-  # best_k_elbow[ii] <- elbow_pt # commented this line out; select elbow manually
-  
-  p <- ggplot(wss_plot, aes(x=x, y=y)) +
-    geom_line() +
-    geom_point() +
-    annotate('point', x = elbow_pt, y = wss[elbow_pt], color = 'green', size = 4) +
-    scale_x_continuous(breaks=1:10) + 
-    labs(title = paste0('Elbow plot for ', names(all_sets)[ii]),
-         x = '# of clusters K',
-         y = 'WSS'
-    )
-    
-  plot_save_path <- paste0('results/', names(all_sets)[ii], '_elbow.png')
-  ggsave(filename = plot_save_path, plot = p, width = 11, height = 5, dpi = 300) # save elbow plots
-}
-
-names(best_k_elbow) <- names(all_sets)
-
-# based on visual examination of elbow plots
-best_k_elbow <- c(9, # freq
-                  9, # freq_plus_one_method
-                  9  # freq_log_foldchange
-)
-
-for (ii in seq_along(all_sets)){
-  km <- kmeans(all_sets[[ii]][-1], centers = best_k_elbow[[ii]], nstart = 25) # [-1] to exclude TCR id
-  set_with_clusters <- all_sets[[ii]]
-  cluster_label <- km$cluster
-  cluster_with_n <- vector(mode='character', length=length(cluster_label))
-  for (jj in seq_along(cluster_label)){
-    cluster_with_n[jj] <- paste0('Cluster_', cluster_label[jj], ' (n=', table(cluster_label)[[cluster_label[jj]]], ')')
-  }
-  set_with_clusters$cluster <- cluster_with_n
+  set_with_clusters$X <- as.factor(X)
   
   # plot clusters to examine trends in each cluster
   freq_long <- set_with_clusters |>
@@ -72,12 +47,11 @@ for (ii in seq_along(all_sets)){
                  names_to = 'Timepoint',
                  values_to = 'Frequency') |>
     mutate(
-      Timepoint = factor(gsub(pattern='Frequency_', replacement='', Timepoint), levels=1:9),
-      X = as.factor(X)
+      Timepoint = factor(gsub(pattern='Frequency_', replacement='', Timepoint), levels=1:9)
     )
   
   if(ii==3){ # for freq_log_foldchange
-    y_lab <- 'Log Fold-change from previous'
+    y_lab <- 'Log Fold-change from Previous'
   }else if(ii %in% c(1,2)){ # freq, freq+1method
     y_lab <- 'Normalized Frequency (z-score)'
   }else{
@@ -92,49 +66,8 @@ for (ii in seq_along(all_sets)){
          x = 'Timepoint',
          y = y_lab
     )
+  p
   
   plot_save_path <- paste0('results/', names(all_sets)[ii], '_cluster_plots.png')
   ggsave(filename = plot_save_path, plot = p, width = 11, height = 5, dpi = 300) # save cluster plots
 }
-
-test_frame <- all_sets[[1]]
-
-# use gap statistic to select number of clusters
-gap_stat <- clusGap(x = test_frame,
-                    FUN = kmeans,
-                    K.max = 20,
-                    nstart = 25,
-                    B = 5
-                    )
-print(gap_stat, method='Tibs2001SEmax')
-optimal_k <- maxSE(gap_stat$Tab[, "gap"],
-                   gap_stat$Tab[, "SE.sim"],
-                   method = "firstSEmax")
-plot(gap_stat)
-# note: trouble with convergence
-
-km <- kmeans(test_frame, centers = optimal_k, nstart = 25)
-set_with_clusters <- test_frame
-set_with_clusters$cluster <- paste0('Cluster_', km$cluster)
-X <- all_sets[[1]][[1]]
-set_with_clusters$X <- X
-
-# plot clusters to examine trends in each cluster
-freq_long <- set_with_clusters |>
-  pivot_longer(cols = starts_with('Frequency_'),
-               names_to = 'Timepoint',
-               values_to = 'Frequency') |>
-  mutate(
-    Timepoint = factor(gsub(pattern='Frequency_', replacement='', Timepoint), levels=1:9),
-    X = as.factor(X)
-  )
-
-p <- ggplot(freq_long, aes(x = Timepoint, y = Frequency, group = X, color = X)) +
-  geom_line(alpha = 0.3) +
-  facet_wrap(~ cluster) +
-  theme(legend.position = 'none') + 
-  labs(title = paste0('Cluster plots for ', names(all_sets)[ii]),
-       x = 'Timepoint',
-       y = 'Normalized Frequency (z-score)'
-  )
-p
